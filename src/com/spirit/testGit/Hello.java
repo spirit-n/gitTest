72b16e362e70d8b10b9302191fade951ecddcfd6
spirit-n <2554532680@qq.com>
2021-04-02 22:30:48 +0800

    第一次提交

---
+++ src/com/spirit/testGit/Hello.java
@@ -0,0 +1,11 @@
+package com.spirit.testGit;
+
+/**
+ * @author spirit
+ * @date 2021/04/02
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
